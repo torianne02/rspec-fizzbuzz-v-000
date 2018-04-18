26103b630bf936e84a084f2ba06647451ad51d8d
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-18 21:49:16 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,10 @@
-def fizzbuzz
-
+def fizzbuzz(int)
+  if int % 3 == 0
+    puts "Fizz"
+  elseif int % 5 == 0
+    puts "Buzz"
+  elseif int % 3 == 0 && int % 5 == 0
+    puts "FizzBuzz"
+  else 
+    puts "nil" 
 end
